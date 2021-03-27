2c8c1de08707a0e9309d1d2270a0f3681e3d9f34
Chris Holliday <chrisuga@me.com>
2021-03-27 02:57:53 -0400

    Added MongoDB creds, connected db

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-console.log("May node be with you.")
-
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+const connectionString = 'mongodb+srv://chollid:chollid@cluster0.v6enn.mongodb.net/QuoteSimulator?retryWrites=true&w=majority'
 
 app.listen(3000, function() {
     console.log("listening on 3000")
 })
 
+app.use(bodyParser.urlencoded({ extended: true}))
+
+// My GET function - sends html to browser
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/index.html')``
+})
+
+// Myh POST function - posts data to DB
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.log(err)
+    console.log("Connected to DataBase")
 })
