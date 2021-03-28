b13559452f4dbe35de2927fae71f48d2808e5052
Chris Holliday <chrisuga@me.com>
2021-03-27 23:50:50 -0400

    Setting up POST functionality

--- server.js
+++ server.js
@@ -5,23 +5,40 @@ const MongoClient = require('mongodb').MongoClient
 
 const connectionString = 'mongodb+srv://chollid:chollid@cluster0.v6enn.mongodb.net/QuoteSimulator?retryWrites=true&w=majority'
 
-app.listen(3000, function() {
-    console.log("listening on 3000")
-})
+app.set('view-engine', 'ejs')
+// app.set("views", "views")
 
-app.use(bodyParser.urlencoded({ extended: true}))
 
-// My GET function - sends html to browser
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')``
-})
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+.then(client=> {
+    const db = client.db('quotes-simulator')
+    const quotesCollection = db.collection('quotes')
+    app.use(bodyParser.urlencoded({ extended: true}))
+    app.use(express.static('public'))
+    // app.use(express.json())
 
-// Myh POST function - posts data to DB
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
+    // My GET function - sends html to browser
+    app.get('/', (req, res) => {
+        quotesCollection.find().toArray()
+        .then(results => {
+            // Pass quotes into the Express' render method
+            res.render('index.ejs', { quotes: results })
+        })
+        .catch(error => console.log(error))
+        res.sendFile(__dirname + '/index.html')
+        res.render('index.ejs', {})
+        res.render(views, local)
+    })
+    // My POST function - posts data to DB
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+        .then(result => {
+            res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+    app.listen(3000, function() {
+        console.log("listening on 3000")
+    })
 })
-
-MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, client) => {
-    if (err) return console.log(err)
-    console.log("Connected to DataBase")
-})
+.catch(error => console.log(error))
